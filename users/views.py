d2e34b47a67ac11e607e37d69bafb73cdff30de7
Stas1407 <stanislaw.rajm@gmail.com>
2020-12-23 13:16:31 +0100

    Started making a profile page

--- users/views.py
+++ users/views.py
@@ -15,24 +15,5 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
-# @login_required
-# def profile(request):
-#     if request.method == 'POST':
-#         u_form = UserUpdateForm(request.POST, instance=request.user)
-#         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-#
-#         if u_form.is_valid() and p_form.is_valid():
-#             u_form.save()
-#             p_form.save()
-#             messages.success(request, f'Your acount has been updated!')
-#             return redirect('profile')
-#     else:
-#         u_form = UserUpdateForm(instance=request.user)
-#         p_form = ProfileUpdateForm(instance=request.user.profile)
-#
-#     context = {
-#         'u_form': u_form,
-#         'p_form': p_form
-#     }
-#
-#     return render(request, 'users/profile.html', context)
+def profile(request):
+    return render(request, 'users/profile.html')
