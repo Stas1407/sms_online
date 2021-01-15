f0da53b511971718a04b3ad0b1616ccb2b81f2bd
Stas1407 <stanislaw.rajm@gmail.com>
2021-01-15 20:00:36 +0100

    Starting conversations via search is now working

--- main/templatetags/getters.py
+++ main/templatetags/getters.py
@@ -19,3 +19,10 @@ def get_id(conversation, user):
 def get_unread_messages(conversation, user):
     uunread_messages_count = conversation.messages.filter(~Q(read_by__in=[user])).count()
     return uunread_messages_count
+
+@register.filter(name="check_if_can_send_message")
+def check_if_can_send_message(messages, user):
+    if len(messages) == 1 and messages[0].author == user:
+        return False
+    else:
+        return True
