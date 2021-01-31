06f192bbc99514e7969e074463677969804d5959
Stas1407 <stanislaw.rajm@gmail.com>
2021-01-31 13:25:48 +0100

    Updated settings

--- sms_online/settings.py
+++ sms_online/settings.py
@@ -135,7 +135,7 @@ MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = '/login'
 LOGIN_URL = '/login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
