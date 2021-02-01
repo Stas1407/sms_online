a0d087094d49fcc91da1fdf45a2d7a0f38d2b971
Stas1407 <stanislaw.rajm@gmail.com>
2021-02-01 12:07:09 +0100

    Changed serving media files

--- main/urls.py
+++ main/urls.py
@@ -15,6 +15,5 @@ urlpatterns = [
     path('delete_message/<int:id>', views.delete_message, name="delete_message")
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
