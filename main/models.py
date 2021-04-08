1e1f8091b30b2ad72ecc693a88f7f28390ad118c
Stas1407 <stanislaw.rajm@gmail.com>
2021-04-08 09:37:47 +0200

    Changed method of saving a group model.

--- main/models.py
+++ main/models.py
@@ -89,11 +89,12 @@ class Group(models.Model):
     def save(self):
         super().save()
 
-        img = Image.open(self.image.path)
-        if img.height > 500 or img.width > 500:
-            output_size = (500, 500)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        if self.image.name != 'default-group.png':
+            img = Image.open(self.image.path)
+            if img.height > 500 or img.width > 500:
+                output_size = (500, 500)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
     
     def delete(self):
         self.last_message = None
